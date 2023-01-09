498db36ce81f8543e7788a5806b193a7835220be
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-08 22:45:01 -0500

    Setting up mongoose connection

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+dotenv.config();
+
 var app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 
 app.use(logger('dev'));
 app.use(express.json());
