d77489561ff60cee4bd3519d3bdd2e68365719ef
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-09 10:35:49 -0500

    Setting up user and auth packages

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, maxLength: 15 },
+  // figure out model for password
+});
+
+// Virtual for user's URL
+UserSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/user/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("User", UserSchema);
